c5d2519341ba367d2ec165fe095ad7bd7d0527e5
Tristan7122 <154232254+Tristan7122@users.noreply.github.com>
2025-04-05 14:46:58 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ OWNER: @darkmakerofc | @MR-NIMA-X
 */
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || 'QUEENELISA;;;==AOzAnZU5WcuplWRNnRnFlbwFlUu12Q540ctRFNnd0a3hkeX9Ed2B1XN91ZjQ1bJJkVrlUMpwd=47678843772',
+  SESSION_ID: process.env.SESSION_ID || 'QUEENELISA;;;==AMa92aRZmaNFEZ0dTThdVaoFXaWdEVk91bWRGNDFHNoZ2QXdGVl5Ud4FVUjkFRRdFbrRmSpwd=47678843772',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name I am Alive Now !*
 
